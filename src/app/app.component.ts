d73d272ac1a921cfe46a9fa6e3b17f993b687ec9
Nicolas Castro <nicolas.castro@coalfire.com>
2025-12-16 22:04:32 -0500

    feat: Implement a welcome banner component to display pending features loaded from a JSON file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksModule } from './tasks/tasks.module';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { WelcomeBannerComponent } from './welcome-banner/welcome-banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, TasksModule, SideMenuComponent],
+  imports: [CommonModule, TasksModule, SideMenuComponent, WelcomeBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/welcome-banner/welcome-banner.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-welcome-banner',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './welcome-banner.component.html',
+    styleUrls: ['./welcome-banner.component.css']
+})
+export class WelcomeBannerComponent implements OnInit {
+    isVisible = true;
+    pendingFeatures: string[] = [];
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit() {
+        this.http.get<any>('/assets/resources.json').subscribe({
+            next: (data) => {
+                if (data && data.pendingFeatures) {
+                    this.pendingFeatures = data.pendingFeatures;
+                }
+            },
+            error: (err) => console.error('Failed to load resources', err)
+        });
+    }
+
+    close() {
+        this.isVisible = false;
+    }
+}
